65481804159909c0e32e99271ec20e3b7cd5ba3d
Cornelia Franta <franta-cornelia@gmx.at>
2020-07-16 16:15:27 +0200

    adding chatbot

--- src/main/java/util/SECRET.java
+++ src/main/java/util/SECRET.java
@@ -1,7 +1,7 @@
 package util;
 
 public class SECRET {
-    private String secret = "NzMzMjUzMjQ1NTM4OTI2Njcy.XxBgWA.goNqRQN5JywCQ-08dqfLvvOv1WI";
+    private String secret = "insert token";
 
 
     public String getSecret() {
